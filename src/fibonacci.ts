e8f772dd62ede7a0cac85dd8ca969ad5a6b64e53
Sergiu Pana <sergiu.pana@softwire.com>
2023-06-29 11:45:01 +0300

    Revert "Merge branch 'handle-negatives'"
    This reverts commit 0790a382df0388ec7a0f4e8984d0019b44b25432, reversing
    changes made to 02b412d03481e0b889f7c26431cfcaf7b440897b.

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,17 +1,10 @@
 export const computeFibonacciNumber = (position: number): number => {
-    if (position === 0) {
-        return 0;
-    }
-    if (position < 0) {
-        return computeNegativeFibonacci(position);
-    }
+    let i = 1;
+    let j = 1;
 
     if (position <= 2) {
         return 1;
     }
-
-    let i = 1;
-    let j = 1;
 
     let currentPosition = 2;
     while (currentPosition < position) {
@@ -22,12 +15,3 @@ export const computeFibonacciNumber = (position: number): number => {
     }
     return j;
 };
-
-const computeNegativeFibonacci = (position: number): number => {
-    if (position >= 0) {
-        throw new Error(`Position must be less than zero! Received: ${position}.`);
-    }
-    const resultIsNegative = position % 2 === 0;
-    const absoluteResult = computeFibonacciNumber(-position);
-    return resultIsNegative ? absoluteResult * -1 : absoluteResult;
-}
